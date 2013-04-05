87abbb4b7d14ffdba72f5c1985301ad6eee7b217
hellwen <hellwen.wu@gmail.com>
2013-04-05 13:09:29 +0800

    init

--- account/models.py
+++ account/models.py
@@ -1,8 +1,22 @@
 from pypackage.extensions import db
 
 
+class PrincipalGroup(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    group_name = db.Column(db.String(50), unique=True)
+    description = db.Column(db.String(500))
+    active = db.Column(db.Boolean, default=True)
+
+    def __unicode__(self):
+        return self.user_name
+
+
 class Principal(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    principalgroup_id = db.Column(db.Integer, db.ForeignKey(PrincipalGroup.id))
+    principalgroup = db.relationship(PrincipalGroup,
+        foreign_keys=principalgroup_id,
+        backref='principalgroup_id+')
     model_name = db.Column(db.String(50), unique=True)
     add = db.Column(db.Boolean, default=False)
     view = db.Column(db.Boolean, default=False)
@@ -11,27 +25,15 @@ class Principal(db.Model):
     active = db.Column(db.Boolean, default=True)
 
     def __unicode__(self):
-        return self.user_name
-
-
-class AccountGroup(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_name = db.Column(db.String(50), unique=True)
-    principal_id = db.Column(db.Integer, db.ForeignKey(Principal.id))
-    principal = db.relationship(Principal, foreign_keys=principal_id,
-        backref='principal_id+')
-    description = db.Column(db.String(500))
-    active = db.Column(db.Boolean, default=True)
-
-    def __unicode__(self):
-        return self.user_name
+        return self.model_name
 
 
 class Account(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_name = db.Column(db.String(50), unique=True)
-    accountgroup_id = db.Column(db.Integer, db.ForeignKey(AccountGroup.id))
-    accountgroup = db.relationship(AccountGroup, foreign_keys=accountgroup_id,
+    principalgroup_id = db.Column(db.Integer, db.ForeignKey(PrincipalGroup.id))
+    principalgroup = db.relationship(PrincipalGroup,
+        foreign_keys=principalgroup_id,
         backref='principal_id+')
     description = db.Column(db.String(500))
     active = db.Column(db.Boolean, default=True)
--- account/views.py
+++ account/views.py
@@ -7,44 +7,51 @@ from .forms import AccountForm
 from pypackage.extensions import db
 from pypackage.base import FormBase
 
-account = Blueprint('account', __name__, url_prefix="/account")
+account = Blueprint('account', __name__,
+    url_prefix="/account",
+    static_folder='static')
 
 
 @account.route("/main/", methods=("GET", "POST"))
 def main():
-    return render_template("hr/main.html")
+    menu = [
+        {"": ("Main")},
+        {"Account": ("Account", "Other")},
+    ]
+    return render_template("main.html", menu=menu)
 
 
 class AccountAdmin(FormBase):
-    list_columns = ("job_name", "description")
+    list_columns = ("user_name", "description")
     fieldsets = [
-        (None, {'fields': ('job_name', 'description')}),
+        (None, {'fields': ('user_name', 'description')}),
     ]
-    column_labels = dict(job_name=_("Job"), description=_("Description"))
+    column_labels = dict(user_name=_("User Name"),
+        description=_("Description"))
 accountadmin = AccountAdmin(account, db.session, Account, AccountForm)
 
 
-@account.route("/account/", methods=("GET", "POST"))
-def job():
+@account.route("/account_list/", methods=("GET", "POST"))
+def account_list():
     return accountadmin.list_view()
 
 
 @account.route("/account/view/id=<int:id>", methods=("GET", "POST"))
-def job_view(id):
+def account_view(id):
     return accountadmin.show_view(id)
 
 
 @account.route("/account/create/", methods=("GET", "POST"))
-def job_create():
+def account_create():
     return accountadmin.create_view()
 
 
 @account.route("/account/edit/id=<int:id>/", methods=("GET", "POST"))
-def job_edit(id):
+def account_edit(id):
     return accountadmin.edit_view(id)
 
 
 @account.route("/account/delete/id=<int:id>/", methods=("GET", "POST"))
-def job_delete(id):
+def account_delete(id):
     return accountadmin.delete_view(id)
 
