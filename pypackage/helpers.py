902e72d25a2c0106083161750f32626c036937b3
hellwen <hellwen.wu@gmail.com>
2013-04-25 22:35:18 +0800

    init

---
+++ pypackage/helpers.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+#coding=utf-8
+
+from flask import g
+from wtforms.validators import Required
+
+
+def set_current_view(view):
+    g._admin_view = view
+
+
+def get_current_view():
+    return getattr(g, '_admin_view', None)
+
+
+def is_required_form_field(field):
+    for validator in field.validators:
+        if isinstance(validator, Required):
+            return True
+    return False
